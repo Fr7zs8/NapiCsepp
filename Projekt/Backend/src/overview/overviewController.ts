c588b60038520614c3533df626a0b87d0d943f1e
sipos.fruzsina <siposfruzsina07@gmail.com>
2026-02-10 14:22:48 +0100

    Overview működik megfelelően

--- Projekt/Backend/src/overview/overviewController.ts
+++ Projekt/Backend/src/overview/overviewController.ts
@@ -1,6 +1,16 @@
 import { Response } from "express";
+import mysql from "mysql2/promise";
+import config from "../config/config";
 
+export async function getEveryThing(_req: any, res: Response) {
+  const connection = await mysql.createConnection(config.database);
+  try {
+    const [result] = (await connection.query("CALL overview()")) as Array<any>;
 
-export async function getEveryThing(_req:any, res:Response){
-    res.status(200).send("Átgonddolásra vár");
+    if (result.length > 0) {
+      res.status(200).send(result[0]);
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
