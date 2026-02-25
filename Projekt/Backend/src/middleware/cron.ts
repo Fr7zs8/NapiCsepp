88cbce69764148609939e56a9bbd315b7191eb27
sipos.fruzsina <siposfruzsina07@gmail.com>
2026-02-25 12:48:54 +0100

    Cron ügyeskedés

--- Projekt/Backend/src/middleware/cron.ts
+++ Projekt/Backend/src/middleware/cron.ts
@@ -5,18 +5,37 @@ export async function resetHabitState() {
   const connection = await mysql.createConnection(config.database);
 
   try {
-    const [results] = await connection.query(
-      "UPDATE activities SET activities.progress_counter = activities.progress_counter + 1 WHERE activities.activity_achive = 1",
-    );
 
-    const [result] = await connection.query(
-      "UPDATE activities SET activities.activity_achive = 0 WHERE activities.activity_type_id = 4",
-    );
+    const [rows] = await connection.query(`
+      SELECT activity_id
+      FROM activities
+      WHERE activity_type_id = 4
+      AND progress_counter <> (DATEDIFF(activity_end_date, activity_start_date) + 1)
+    `) as Array<any>;
 
-    console.log(result);
-    console.log(results);
+    if (rows.length === 0) {
+      console.log("Minden progress elérte a maximumot. Nem fut le.");
+      return;
+    }
+
+    await connection.query(`
+      UPDATE activities
+      SET progress_counter = progress_counter + 1
+      WHERE activity_achive = 1
+      AND progress_counter <> (DATEDIFF(activity_end_date, activity_start_date) + 1)
+    `);
+
+    await connection.query(`
+      UPDATE activities
+      SET activity_achive = 0
+      WHERE activity_type_id = 4
+    `);
+
     console.log("A cron lefutott");
+
   } catch (e) {
     console.log(e);
+  } finally {
+    await connection.end();
   }
 }
