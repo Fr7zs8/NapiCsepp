28d00980855c5f87d73427e3a1c317923184e188
Fr7zs8 <siposfruzsina07@gmail.com>
2026-03-01 19:57:00 +0100

    Valami hiba van de plusz adatok elkészitve

--- Projekt/Backend/src/app/app.ts
+++ Projekt/Backend/src/app/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-cron.schedule("* * * * *", () => {
+cron.schedule("0 0 * * *", () => {
   resetHabitState();
 });
 
